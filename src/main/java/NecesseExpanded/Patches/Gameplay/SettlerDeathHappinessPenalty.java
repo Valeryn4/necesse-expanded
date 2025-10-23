ebc8808fdd31eb0b7e568d8e1076aea9ee0f6cf6
GC <gareth2.chappell@live.uwe.ac.uk>
2025-10-23 15:45:25 +0100

    Implemented settler happiness penalty when other settlers die.

--- src/main/java/NecesseExpanded/Patches/Gameplay/SettlerDeathHappinessPenalty.java
+++ src/main/java/NecesseExpanded/Patches/Gameplay/SettlerDeathHappinessPenalty.java
@@ -1,26 +1,28 @@
 package NecesseExpanded.Patches.Gameplay;
 
+import java.util.HashSet;
+
 import necesse.engine.modLoader.annotations.ModMethodPatch;
+import necesse.entity.mobs.Attacker;
 import necesse.entity.mobs.buffs.ActiveBuff;
 import necesse.entity.mobs.friendly.human.HumanMob;
 import necesse.level.maps.levelData.settlementData.LevelSettler;
 import net.bytebuddy.asm.Advice.OnMethodExit;
 import net.bytebuddy.asm.Advice.This;
 
-@ModMethodPatch(target = HumanMob.class, name = "onDeath", arguments = {})
+@ModMethodPatch(target = HumanMob.class, name = "onDeath", arguments = {Attacker.class, HashSet.class})
 public class SettlerDeathHappinessPenalty 
 {
     @OnMethodExit
     static void onExit(@This HumanMob ThisSettler)
     {
-        if (ThisSettler.getWorldEntity() != null)
+        System.out.println("TEST " + ThisSettler.getMob().getDisplayName());
+        if (ThisSettler.getSettlementServerData() != null)
         {
-            if (ThisSettler.getSettlementServerData() != null)
+            for (LevelSettler Settler : ThisSettler.getSettlementServerData().settlers)
             {
-                for (LevelSettler Settler : ThisSettler.getSettlementServerData().settlers)
-                {
-                    Settler.getMob().getMob().buffManager.addBuff(new ActiveBuff("settler_death_penalty", ThisSettler, 600f, ThisSettler), true);
-                }
+                System.out.println("TESTSTRING " + Settler.getMob().getSettlerName());
+                Settler.getMob().getMob().buffManager.addBuff(new ActiveBuff("settler_death_penalty", Settler.getMob().getMob(), 600f, Settler.getMob().getMob()), true);
             }
         }
     }
